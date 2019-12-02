099229e6b886ce9e09108244b154236054e2a307
James <14856717+ZPanic0@users.noreply.github.com>
2019-12-02 02:33:01 -0500

    Add solution for 2.2

---
+++ 2.2/ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var upperLimit = 99;
+            var input = File
+                .ReadAllText("input.txt")
+                .Split(',')
+                .Select(input => int.Parse(input))
+                .ToArray();
+
+            for (int noun = 0; noun < upperLimit; noun++)
+            {
+                for (int verb = 0; verb < upperLimit; verb++)
+                {
+                    var freshMemory = (int[])input.Clone();
+
+                    freshMemory[1] = noun;
+                    freshMemory[2] = verb;
+
+                    var result = Intcode.Process(freshMemory);
+
+                    if (result == 19690720)
+                    {
+                        Console.WriteLine(100 * noun + verb);
+                    }
+                }
+            }
+        }
+    }
+}
