e788d621f2738b34b0c19c49f03b60d7239542b3
James <14856717+ZPanic0@users.noreply.github.com>
2019-12-04 01:57:25 -0500

    Add solution for 4.1

---
+++ 4.1/ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matchCount = 0;
+
+            for (int testNumber = 236491; testNumber <= 713787; testNumber++)
+            {
+                matchCount += MatchesRules(testNumber) ? 1 : 0;
+            }
+
+            Console.WriteLine(matchCount);
+        }
+
+        private static bool MatchesRules(int testNumber)
+        {
+            var partialNumber = testNumber;
+            var foundConsecutiveMatch = false;
+            for (int counter = 0; counter < 5; counter++)
+            {
+                var currentDigit = partialNumber % 10;
+                partialNumber /= 10;
+                var nextDigit = partialNumber % 10;
+
+                if (currentDigit < nextDigit)
+                {
+                    return false;
+                }
+
+                if (currentDigit == nextDigit)
+                {
+                    foundConsecutiveMatch = true;
+                }
+            }
+
+            return foundConsecutiveMatch;
+        }
+    }
+}
